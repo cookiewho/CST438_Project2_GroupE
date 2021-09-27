8330eca5a70d5922a1cbdb070df07942b14d4a6d
Jamiel Capatayan <26446581+JamielCapatayan@users.noreply.github.com>
2021-09-27 11:02:31 -0700

    made new route for all items

--- wishlist/home/urls.py
+++ wishlist/home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home-page'),
+    path('items/', views.ListAllItems, name='items-page')
 ]
--- wishlist/home/views.py
+++ wishlist/home/views.py
@@ -14,6 +14,20 @@ posts = [
         'publishing_date' : 'September 25, 2021'
     }
 ]
+
+items = [
+    {
+        'item_no' : '1',
+        'item_name' : 'Water Bottle',
+        'content' : 'drink water duh',
+    },
+    {
+        'item_no' : '2',
+        'item_name' : 'Phone',
+        'content' : 'its a phone, what did you expect?',
+    }
+]
+
 # Create your views here.
 def home(request):
     context = {
@@ -21,4 +35,10 @@ def home(request):
     }
     return render(request, 'home/home.html', context)
 
-#{'title' : ''}
+
+def ListAllItems(request):
+    # pull from db
+    context = {
+        'items' : items
+    }
+    return render(request, 'home/items.html', context)
