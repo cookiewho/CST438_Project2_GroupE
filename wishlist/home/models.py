804aeeb489a86184785e3c45c821ab5b39166672
Jamiel Capatayan <26446581+JamielCapatayan@users.noreply.github.com>
2021-09-28 20:31:24 -0700

    added Item and itemList models.

--- wishlist/home/models.py
+++ wishlist/home/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class List(models.Model): 
+    list_num = models.IntegerField()
--- wishlist/items/models.py
+++ wishlist/items/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from home.models import List
 
-
-# List is a list of lists (userList, itemList)
-   
+# if tables need to be updated, change the model,then run python manage.py migrate
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    pass
+
+# # List is a list of lists (userList, itemList)
+class itemList(models.Model):
+    list_id = models.ForeignKey(List, on_delete=CASCADE)
+    item_id = models.ManyToManyField(Item)
