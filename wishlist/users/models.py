6ed2db978d751018c14abfd26e9d8a00eb033595
Jamiel Capatayan <26446581+JamielCapatayan@users.noreply.github.com>
2021-09-28 22:31:34 -0700

    created User and userList models

--- wishlist/users/models.py
+++ wishlist/users/models.py
@@ -1,11 +1,19 @@
-from typing import DefaultDict
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from home.models import List
 
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+    pass
+
+class userList(models.Model):
+    user_id = models.ForeignKey(User, on_delete=CASCADE)
+    list_id = models.ForeignKey(List, on_delete=CASCADE)
