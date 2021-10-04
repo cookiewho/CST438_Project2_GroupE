f5f22cf34c14264e706329b8dbefe746157d89a6
Jamiel Capatayan <26446581+JamielCapatayan@users.noreply.github.com>
2021-10-04 14:38:54 -0700

    added category and description field to Item model

--- wishlist/items/models.py
+++ wishlist/items/models.py
@@ -8,6 +8,8 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     image = models.URLField(default='None')
+    category = models.CharField(max_length=100, default='None')
+    description = models.TextField(default='None')
 
     def __str__(self):
         return self.name
