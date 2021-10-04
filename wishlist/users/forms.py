c369c7edc3f55377f0a13c18e58d41be149b463a
Nayeli De Jesus <nayelidjm142@gmail.com>
2021-10-04 00:14:28 -0700

    Created form.py and UserUpdateForm class

---
+++ wishlist/users/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+#firstname
+#lastname
+#username
+#password
