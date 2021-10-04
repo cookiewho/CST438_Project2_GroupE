1aa13892cc786640a08b33d390bbf25b4501862a
Jamiel Capatayan <26446581+JamielCapatayan@users.noreply.github.com>
2021-10-04 14:38:54 -0700

    added category and description field to Item model

--- wishlist/items/views.py
+++ wishlist/items/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render
+# from models import Item
 
 def ListAllItems(request):
     # pull from db
-    items = [
-        {
-            'item_no' : '1',
-            'item_name' : 'Water Bottle',
-            'content' : 'drink water duh',
-        },
-        {
-            'item_no' : '2',
-            'item_name' : 'Phone',
-            'content' : 'its a phone, what did you expect?',
-        }
-    ]
+    items = []
+
 
     context = {
         'items' : items
