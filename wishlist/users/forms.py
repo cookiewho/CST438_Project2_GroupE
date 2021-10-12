a167a3e3c5eaf20cef49bc3c604609cfc57f8878
Nayeli De Jesus <nayelidjm142@gmail.com>
2021-10-12 01:31:56 -0700

    Added firstname and last name input

--- wishlist/users/forms.py
+++ wishlist/users/forms.py
@@ -4,8 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Update
 
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
+    usernameEdit = forms.CharField(label = 'New username', max_length=100)
+    firstNameEdit = forms.CharField(label = 'First name', max_length=100)
+    lastNameEdit = forms.CharField(label = 'Last name', max_length=100)
+
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['usernameEdit', 'firstNameEdit', 'lastNameEdit']
