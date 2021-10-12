60385c8ae1695a45d99275fe2df15edb4016ceef
Nayeli De Jesus <nayelidjm142@gmail.com>
2021-10-12 10:58:54 -0700

    added password input field

--- wishlist/users/forms.py
+++ wishlist/users/forms.py
@@ -6,8 +6,9 @@ from .models import Update
 class UserUpdateForm(forms.ModelForm):
     firstNameEdit = forms.CharField(label = 'First name', max_length=100)
     lastNameEdit = forms.CharField(label = 'Last name', max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput())
 
 
     class Meta:
         model = User
-        fields = ['username', 'firstNameEdit', 'lastNameEdit']
+        fields = ['firstNameEdit', 'lastNameEdit', 'username', 'password']
