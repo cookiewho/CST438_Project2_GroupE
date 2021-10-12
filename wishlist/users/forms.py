f76de0bc28e6da5de7f3c28fbe68b0e390880e3c
Nayeli De Jesus <nayelidjm142@gmail.com>
2021-10-12 01:34:56 -0700

    removed usernameEdit

--- wishlist/users/forms.py
+++ wishlist/users/forms.py
@@ -4,11 +4,10 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Update
 
 class UserUpdateForm(forms.ModelForm):
-    usernameEdit = forms.CharField(label = 'New username', max_length=100)
     firstNameEdit = forms.CharField(label = 'First name', max_length=100)
     lastNameEdit = forms.CharField(label = 'Last name', max_length=100)
 
 
     class Meta:
         model = User
-        fields = ['usernameEdit', 'firstNameEdit', 'lastNameEdit']
+        fields = ['username', 'firstNameEdit', 'lastNameEdit']
