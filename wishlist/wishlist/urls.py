015aa7011ff8778d7bf8d97b477f0480b44693e3
cookiewho <dinkwho@gmail.com>
2021-10-15 22:03:45 -0700

    Attempting to implement API

--- wishlist/wishlist/urls.py
+++ wishlist/wishlist/urls.py
@@ -19,17 +19,17 @@ from django.urls import path, include
 from items import views as item_views
 from django.shortcuts import redirect
 from rest_framework.authtoken.views import obtain_auth_token
-# from users import views as user_views
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('addItems/', addItems_views.addItems, name='addItems'),
     path('', include('home.urls'), name='home'),
     path('items/', item_views.ListAllItems),
-    # path('register/', user_views.register, name='register'),
-    # path('login/', user_views.login, name='login'),
-    # path('update/', user_views.update, name='update'),
-    # path('accountDetails/', user_views.account, name='accountDetails'),
+    path('register/', user_views.register, name='register'),
+    path('login/', user_views.login, name='login'),
+    path('update/', user_views.update, name='update'),
+    path('accountDetails/', user_views.account, name='accountDetails'),
 
     #REST API URL'S
     path('api/', include('api.urls')),
