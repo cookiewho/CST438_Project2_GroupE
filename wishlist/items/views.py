aa398af60b00010d03d1d6fcb973d4011910f509
Jamiel Capatayan <26446581+JamielCapatayan@users.noreply.github.com>
2021-10-18 13:13:28 -0700

    created working view for a specific item

--- wishlist/items/views.py
+++ wishlist/items/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from api import views
 
 def ListAllItems(request):
-    # pull from db
     context = {}
     
     response = views.view_items(request)
     context = {'items' : response.data}
 
     return render(request, 'items/items.html', context)
+
+def ShowItem(request, item_id):
+    print('==== ENTERED FUNCTION ====')
+    return render(request, 'items/item.html')
