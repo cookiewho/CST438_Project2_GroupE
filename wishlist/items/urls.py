e495944f68ed84d8974c38a2a47b5e925d120f15
Jamiel Capatayan <26446581+JamielCapatayan@users.noreply.github.com>
2021-10-19 11:47:06 -0700

    fixed button functionality

--- wishlist/items/urls.py
+++ wishlist/items/urls.py
@@ -7,5 +7,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('items/', item_views.ListAllItems),
-    path('items/<int:item_id>', item_views.ShowItem, name="specific_item")
+    path('items/<int:item_id>', item_views.ShowItem, name="specific_item"),
+    path('items/<int:item_id>user=<int:user_id>', item_views.AddItem, name="add_item")
+    # url(r'^items/(\d+)$', 'item_views.AddItem', name="add_item")
 ]
--- wishlist/items/views.py
+++ wishlist/items/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from api import views as api
 
 def ListAllItems(request):    
@@ -9,6 +10,23 @@ def ListAllItems(request):
 
 def ShowItem(request, item_id):
     response = api.view_items_by_id(request, item_id)
-    context = {'item':response.data}
+
+    request.session['user_id'] = 1
+    user_id = request.session['user_id']
+    
+    context = {'item' : response.data, 'user' : user_id}
 
     return render(request, 'items/item.html', context)
+
+def AddItem(request, item_id, user_id):
+    print(" ==== ENTERED FUNCTION ====")
+    
+    # call view all user lists
+    # iterate and find matching user list with user_id
+    # get user_list id
+    # call api function to add item to user list
+
+    # response = api.create_item_by_user(request, item_id, user_id)
+    context = {'item' : response.data}
+
+    return redirect("/")
